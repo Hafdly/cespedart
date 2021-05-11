cb656cb13d4664b270b895f5f663e48b8602b58e
Navaiklam <dmlk.dark@gmail.com>
2021-05-11 15:13:36 +0200

    Menu & Foot

--- js/gallery.js
+++ js/gallery.js
@@ -2,3 +2,9 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+
+$('.sub-menu ul').hide();
+$(".sub-menu a").click(function () {
+  $(this).parent(".sub-menu").children("ul").slideToggle("200");
+  $(this).find("i.fa").toggleClass("fa-angle-up fa-angle-down");
+});
